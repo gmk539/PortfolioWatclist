6cd363b1cd751063746e7c15105080272b1e0ee1
Manikanta Gudipudi <mgudipudi@factset.com>
2020-04-14 13:05:20 +0530

    changed menu label

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,7 +2,7 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-     title: 'My Watchlist',
+     title: 'My Recommendation list',
      icon:'eye',
      link: '/pages/layout/list'
   },
